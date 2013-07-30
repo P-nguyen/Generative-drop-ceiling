275f34bd89eba9cec19be3a2451a43d804b82ec7
Peter <panguyenuwm@gmail.com>
2013-07-29 18:31:46 -0700

    transaction test bugs

---
+++ Python/get_geometry_transaction_test_2.py
@@ -0,0 +1,21 @@
+﻿"""
+# be sure to include this so everything works in Dynamo Revit
+import sys
+path = 'C:\\dev\\Dynamo\\bin\\Release'
+sys.path.append(path)
+import clr
+clr.AddReference('LibGNet')
+# be sure to include this so you have code completion
+from Autodesk.LibG import *
+from Autodesk.LibG import Point,Line,Surface,Polygon,Geometry,Cuboid,CoordinateSystem,Plane,Vector
+"""
+
+
+elem = IN[1][0]
+ 
+output = []
+
+for f in elem.Faces:
+    output.append(f.Area)
+
+OUT = output
